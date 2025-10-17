e98945bf346c6519338a9bf082ce230b78a412af
Ronald <romanuelcatun13@gmail.com>
2025-10-16 20:20:51 -0600

    Add PhotoApplication
    Removed comments from the PhotoApplication class for clarity.

---
+++ app/src/main/java/com/example/tareadeoptimizacion/PhotoApplication.kt
@@ -0,0 +1,40 @@
+package com.example.tareaoptimizacion
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
+
+
+class PhotoApplication : Application(), ImageLoaderFactory {
+    
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(250 * 1024 * 1024) // 250 MB
+                    .build()
+            }
+            // Políticas de caché
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .networkCachePolicy(CachePolicy.ENABLED)
+            .crossfade(true)
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    logger(DebugLogger())
+                }
+            }
+            .allowHardware(true)
+            .build()
+    }
+}
