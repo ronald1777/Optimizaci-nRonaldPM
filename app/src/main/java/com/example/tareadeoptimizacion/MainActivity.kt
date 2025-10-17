650560f934d2751c4f0688190d47bfc2a70a23bd
Ronald <romanuelcatun13@gmail.com>
2025-10-16 20:09:41 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/tareadeoptimizacion/MainActivity.kt
+++ app/src/main/java/com/example/tareadeoptimizacion/MainActivity.kt
@@ -1,17 +1,15 @@
-package com.example.tareadeoptimizacion
+package com.example.tareaoptimizacion
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tareadeoptimizacion.ui.theme.TareaDeOptimizacionTheme
+import com.example.tareaoptimizacion.ui.navigation.AppNavigation
+import com.example.tareaoptimizacion.ui.theme.TareaDeOptimizacionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TareaDeOptimizacionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TareaDeOptimizacionTheme {
-        Greeting("Android")
     }
 }
